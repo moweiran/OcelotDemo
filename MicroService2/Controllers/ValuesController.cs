72130572419fca38fbb77f020207cdb82b0b5109
56cargo-lzy <lzy@56cargo.com>
2020-08-19 19:02:34 +0800

    update

---
+++ MicroService2/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "microservice2";
+        }
+    }
+}
